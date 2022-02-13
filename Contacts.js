c9e4fd27de3b9755efc0a5667a9b0a1e8c02264d
itzrizvi <shahriar.me.work@gmail.com>
2022-02-13 23:46:49 +0600

    get Create update function done

--- Contacts.js
+++ Contacts.js
@@ -20,5 +20,16 @@ class Contacts {
     updateContactById(id, updatedContact) {
         let index = this.contacts.findIndex(contact => contact.id === id);
 
+        this.contacts[index].name = updatedContact.name || this.contacts[index].name;
+        this.contacts[index].phone = updatedContact.phone || this.contacts[index].phone;
+        this.contacts[index].email = updatedContact.email || this.contacts[index].email;
+
+        return this.contacts[index];
+    }
+
+
+    deleteContactById(id) {
+        let index = this.contacts.findIndex(contact => contact.id === id);
+
     }
 }
