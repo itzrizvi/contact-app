81ac94cd78bc4f486815df475d9e83814e74034a
itzrizvi <shahriar.me.work@gmail.com>
2022-02-15 00:53:08 +0600

    Contact routes done

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 // Required Packages
 const express = require("express");
+const morgan = require('morgan');
+const contactRoutes = require('/contactRoutes');
+
+
+
 const app = express();
+app.use(morgan('dev'));
+
+
+// Routes
+app.use('/contacts', contactRoutes);
+
+app.get('*', (req, res) => {
+    res.send(<h1>Please Use The Correct Routes</h1>);
+})
 
 
 // PORT Handle
